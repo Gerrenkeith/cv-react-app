143d4f8967031f9f813070cf8ddfca709072a395
Gerren Keith <gerren.keith23@gmail.com>
2024-10-24 21:55:34 -0500

    Cv body
    add the body to the page in a the Body component to contain
    information about my education in web development

--- src/Body.jsx
+++ src/Body.jsx
@@ -10,17 +10,42 @@ function Education(){
                     <div>
                         <p>Jul 2021 - Nov 2022</p>
                     </div>
-                    <div></div>
+                    <div>
+                        <h3>FlatIron School</h3>
+                        <li>Completed intensive full-stack web development program covering HTML, CSS, JavaScript, React, SQLite, and Ruby on Rails.</li>
+                        <li>Developed proficiency in front-end and back-end technologies through hands-on projects and coding exercises.</li>
+                        <li>Acquired practical skills in building responsive web applications and utilizing databases for dynamic content management.</li>
+                    </div>
                 </div>
                 <div>
-                    <div></div>
-                    <div></div>
+                    <div>
+                        <p>Aug 2023 - Oct 2023</p>
+                    </div>
+                    <div>
+                        <h3>Coursera</h3>
+                        <label>Certifications Received:</label>
+                        <li>HTML </li>
+                        <li>CSS</li>
+                        <li> JavaScript</li>
+                        <li>Responsive Web Design</li>
+                    </div>
                 </div>
                 <div>
-                    <div></div>
-                    <div></div>
+                    <div>
+                        <p>Jan 2024-June 2024</p>
+                    </div>
+                    <div>
+                        <p>The Oden Project</p>
+                        <p>Professional Development:</p>
+                        <li>Engaged in self-directed learning through The Odin Project, a comprehensive curriculum for full-stack web development.</li>
+                        <li>Expanded proficiency in HTML, CSS, and JavaScript by completing practical exercises, projects, and challenges.</li>
+                        <li>Developed problem-solving skills and gained hands-on experience in building interactive web applications.</li>
+                        <li>Demonstrated commitment to continuous learning and personal development in the field of web development.</li>
+                        <li>Learned how to work better with Git and Github to collaborate with a team.</li>
+                    </div>
                 </div>
             </div>
+            <hr/>
         </>
     )
 }
